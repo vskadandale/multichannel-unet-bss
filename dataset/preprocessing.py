0fb7a680c0d98ea592927198ffaf6b7dbb24d49e
Juan Montesinos <kvsphantom@gmail.com>
2020-01-20 21:36:44 +0100

    added code for data preprocessing (added last chunk in case of testset). seems Cto be working. setting momentum=0.9, LR=0.01

---
+++ dataset/preprocessing.py
@@ -0,0 +1,114 @@
+import numpy as np
+import sys
+
+sys.path.append('../')
+from functools import partial
+import librosa
+import torch
+from utils import create_folder
+import librosa.display
+from sklearn.model_selection import train_test_split
+import shutil
+import csv
+from settings import *
+
+
+def get_sources(folder):
+    y = [librosa.load(os.path.join(folder, element + '.wav'),
+                      sr=ORIGINAL_SAMPLING_RATE)[0] for element in [*SOURCES, 'mixture']]
+    downsampled = np.stack(list(map(partial(librosa.core.resample,
+                                            orig_sr=ORIGINAL_SAMPLING_RATE,
+                                            target_sr=TARGET_SAMPLING_RATE), y)))
+    return downsampled
+
+
+def split_sources(sources, flag):
+    window = int(TARGET_SAMPLING_RATE * DURATION)
+    M = (np.max(sources.shape) // window)
+    if flag == 'train':
+        splits = sources[..., :M * window]
+        splits = np.reshape(splits, splits.shape[:-1] + (M, window))
+    else:
+        zero_padding = window - np.max(sources.shape) % window
+        splits = np.concatenate([sources, np.zeros([len(SOURCES), zero_padding])], axis=1)
+        splits = np.reshape(splits, splits.shape[:-1] + (M + 1, window))
+    return splits
+
+
+def _stft(sources):
+    s = torch.from_numpy(sources).float().cuda(cuda)
+    shape = s.size()
+    with torch.no_grad():
+        stft_output = torch.stft(s.view(-1, shape[-1]),
+                                 n_fft=NFFT,
+                                 hop_length=HOP_LENGTH,
+                                 window=torch.hann_window(NFFT).cuda(cuda))
+        stft = stft_output.view(*shape[:-1], *stft_output.size()[1:3], 2).data.cpu().numpy()
+    stft = stft[..., 0] + stft[..., 1] * 1j
+    return stft
+
+
+def get_signal_energy(signal):
+    return sum(abs(signal) ** 2)
+
+
+def save_chunks(chunk_id, subset_type, track_name, sources, energy_profile):
+    save_folder_path = os.path.join(CHUNKS_PATH, subset_type, track_name, str(chunk_id))
+    create_folder(save_folder_path)
+    true_label = np.zeros(len(SOURCES) + 1, dtype='int')
+    for source_id, source in enumerate([*SOURCES, 'MIX']):
+        signal = sources[source_id, chunk_id]
+        signal_energy = get_signal_energy(signal)
+        if int(signal_energy) > ENERGY_THRESHOLD:
+            true_label[source_id] = 1
+        save_path = os.path.join(save_folder_path, source + '_' + str(int(round(signal_energy))) + '.wav')
+        energy_profile[source_id][os.path.dirname(save_path)] = signal_energy
+        librosa.output.write_wav(save_path, signal, TARGET_SAMPLING_RATE)
+    return energy_profile, true_label[:-1]
+
+
+VALIDATION_PATH = os.path.join(MUSDB_SPLITS_PATH, 'val')
+train_paths = []
+energy_profile = [{} for _ in range(len(SOURCES) + 1)]
+sample_dict = {}
+subset = ['train', 'test']
+cuda = 0
+
+for subset_type in subset:
+    DATA_PATH = os.path.join(MUSDB_WAVS_FOLDER_PATH, subset_type)
+    tracks = sorted(os.listdir(DATA_PATH))
+
+    for track_id, track_name in enumerate(tracks):
+        track_path = os.path.join(DATA_PATH, track_name)
+        dump_path = os.path.join(MUSDB_SPLITS_PATH, subset_type, track_name)
+        create_folder(dump_path)
+        sources_downsampled = get_sources(track_path)
+        sources_split = split_sources(sources_downsampled, subset_type)
+        stft_output = _stft(sources_split)
+        for chunk_id in range(stft_output.shape[1]):
+            matrix = stft_output[:, chunk_id, ...]
+            energy_profile, true_label = save_chunks(chunk_id, subset_type, track_name, sources_split, energy_profile)
+            if subset_type == 'train':
+                train_paths.append(os.path.join(dump_path, str(chunk_id) + '.npy'))
+            sample_dict['spec'] = matrix
+            sample_dict['true_label'] = true_label
+            full_path = os.path.join(dump_path, str(chunk_id))
+            np.save(full_path, sample_dict)
+            print('[{0}/{1}] || [{2}||{3}]'.format(chunk_id + 1, stft_output.shape[1], track_id + 1, len(tracks)))
+        del sources_split
+
+create_folder(ENERGY_PROFILE_FOLDER)
+for source_id, source in enumerate([*SOURCES, 'MIX']):
+    with open(os.path.join(ENERGY_PROFILE_FOLDER, source + '_energy_profile.csv'), 'w') as f:
+        w = csv.writer(f)
+        w.writerows(energy_profile[source_id].items())
+    np.save(os.path.join(ENERGY_PROFILE_FOLDER, source + '_energy_profile'), energy_profile[source_id])
+
+########## CREATING TRAINING-VALIDATION SPLIT##############
+X_train, X_val = train_test_split(train_paths, test_size=0.05, random_state=0)
+create_folder(VALIDATION_PATH)
+
+for file in X_val:
+    val_path = str.replace(file, 'train', 'val')
+    create_folder(os.path.abspath(os.path.join(val_path, os.pardir)))
+    shutil.move(file, val_path)
--- settings.py
+++ settings.py
@@ -1,47 +1,47 @@
 import os
 
+
 def set_path(path):
     if not os.path.exists(path):
         os.makedirs(path)
     return path
 
-TYPE='4src'#'4src'
-ISOLATED_SOURCE_ID=0
-SOURCES=['vocals', 'accompaniment', 'drums', 'bass', 'other']
-if TYPE=='2src':
-    SOURCES_SUBSET=['vocals', 'accompaniment']
-else:
-    SOURCES_SUBSET=['vocals', 'drums', 'bass', 'other']
 
-TARGET_SAMPLING_RATE=10880
-DURATION=6                                                      #in seconds
-NFFT=1022
-HOP_LENGTH=256
-STFT_WIDTH=int((TARGET_SAMPLING_RATE * DURATION / HOP_LENGTH) + 1)   # 256=(10880x6/256)+1
+TYPE = '4src'  # '4src'
+ISOLATED_SOURCE_ID = 0
+SOURCES = ['vocals', 'accompaniment', 'drums', 'bass', 'other']
+if TYPE == '2src':
+    SOURCES_SUBSET = ['vocals', 'accompaniment']
+else:
+    SOURCES_SUBSET = ['vocals', 'drums', 'bass', 'other']
+
+ORIGINAL_SAMPLING_RATE = 44100
+TARGET_SAMPLING_RATE = 10880
+DURATION = 6  # in seconds
+NFFT = 1022
+HOP_LENGTH = 256
+STFT_WIDTH = int((TARGET_SAMPLING_RATE * DURATION / HOP_LENGTH) + 1)  # 256=(10880x6/256)+1
 
 K = len(SOURCES_SUBSET)  # Number of instruments
-#self.rootdir = set_path(EXPERIMENTS_FOLDER)
 BATCH_SIZE = 16
-LR = 0.001                          # \\TODO 0.01
+LR = 0.01
 EPOCHS = 60000
 DWA_TEMP = 2
-MOMENTUM = 0                        # \\TODO 0.9
+MOMENTUM = 0.9
 WEIGHT_DECAY = 0
 INITIALIZER = 'xavier'
 OPTIMIZER = 'SGD'
 USE_BN = True
-PRETRAINED=None                                                 ### None or True ???????????????
-#self.datadir = MUSDB_SPLITS_PATH
-TRACKGRAD=False
+PRETRAINED = None
+TRACKGRAD = False
 ACTIVATION = None
 INPUT_CHANNELS = 1
 
 ###### WEIGHTS #######
-PRETRAINED_UNET_CONFIG='2019-12-01 15:01:42'
+PRETRAINED_UNET_CONFIG = '2019-12-01 15:01:42'
 
 #### TENSORBOARD CONFIG #####
-PARAMETER_SAVE_FREQUENCY=100
-
+PARAMETER_SAVE_FREQUENCY = 100
 
 """
 MUSDB_FOLDER_PATH='/media/venkatesh/slave/dataset/musdb'
@@ -53,42 +53,37 @@ OUTPUT_PATH='/media/venkatesh/slave/dataset/output'
 RESULTS_PATH='/media/venkatesh/slave/dataset/results'
 """
 
-MUSDB_FOLDER_PATH='/mnt/DATA/datasets/musdb/'
-INDIAN_SAMPLE_DATA='/mnt/DATA/datasets/Indian_Music/sample/X'
-EXPERIMENTS_FOLDER='/mnt/DATA/weights'
-DUMPS_FOLDER='/mnt/DATA/dumps'
-SEPERATED_OUTPUT_PATH='/mnt/DATA/datasets/musdb/output_crumbs'
-OUTPUT_PATH='/mnt/DATA/datasets/musdb/output'
-RESULTS_PATH='/mnt/DATA/datasets/musdb/results'
-
-
-
+MUSDB_FOLDER_PATH = '/mnt/DATA/datasets/musdb/'
+INDIAN_SAMPLE_DATA = '/mnt/DATA/datasets/Indian_Music/sample/X'
+EXPERIMENTS_FOLDER = '/mnt/DATA/weights'
+DUMPS_FOLDER = '/mnt/DATA/dumps'
+SEPERATED_OUTPUT_PATH = '/mnt/DATA/datasets/musdb/output_crumbs'
+OUTPUT_PATH = '/mnt/DATA/datasets/musdb/output'
+RESULTS_PATH = '/mnt/DATA/datasets/musdb/results'
 
 ROOT_DIR = set_path(EXPERIMENTS_FOLDER)
-PRETRAINED_UNET_WEIGHTS_PATH=os.path.join(EXPERIMENTS_FOLDER,PRETRAINED_UNET_CONFIG,'bestcheckpoint.pth')
-TEST_UNET_CONFIG='2020-01-17 14:15:31'#'2020-01-03 11:42:35'#'2020-01-02 19:19:54'#'baseline'#'2020-01-01 20:03:30'#'2019-12-31 14:27:24'#'2019-12-18 18:53:17'
-TEST_UNET_WEIGHTS_PATH=os.path.join(EXPERIMENTS_FOLDER,TEST_UNET_CONFIG,'bestcheckpoint.pth')
-TEST_UNET_REFINED_CONFIG='2019-12-18 18:53:17'
-TEST_UNET_REFINED_WEIGHTS_PATH=os.path.join(EXPERIMENTS_FOLDER,TEST_UNET_REFINED_CONFIG,'bestcheckpoint.pth')
-RAW_MUSDB_PATH=os.path.join(MUSDB_FOLDER_PATH,'musdb18')
-MUSDB_WAVS_FOLDER_PATH=os.path.join(MUSDB_FOLDER_PATH,'musdb18_wavs')
-ENERGY_PROFILE_FOLDER=os.path.join(MUSDB_FOLDER_PATH,'energy_profile')
-MUSDB_SPLITS_PATH=os.path.join(MUSDB_FOLDER_PATH,'musdbsplit')
-MUSDB_SPLITS_AUG_PATH=os.path.join(MUSDB_FOLDER_PATH,'musdbaug')
-CHUNKS_PATH=os.path.join(MUSDB_FOLDER_PATH,'musdb_chunks')
-SPECTROGRAMS_PATH=os.path.join(MUSDB_FOLDER_PATH,'musdb_spectrograms')
-SOURCE_MIX_PATH=os.path.join(MUSDB_FOLDER_PATH,'musdb_smix')
-STEMS_PATH=os.path.join(MUSDB_FOLDER_PATH,'musdb_stems')
-RECON_GT_PATH=os.path.join(MUSDB_FOLDER_PATH,'musdbGT')
-SOURCE_ESTIMATES_PATH=os.path.join(MUSDB_FOLDER_PATH,'eval')
-TEST_DATA_PATH=os.path.join(RAW_MUSDB_PATH,'test')
-TEST_SPEC_DATA_PATH=os.path.join(MUSDB_SPLITS_PATH,'test')
-TEST_MAPPINGS_PATH=os.path.join(os.path.dirname(SEPERATED_OUTPUT_PATH),'test_mappings.npy')
+PRETRAINED_UNET_WEIGHTS_PATH = os.path.join(EXPERIMENTS_FOLDER, PRETRAINED_UNET_CONFIG, 'bestcheckpoint.pth')
+TEST_UNET_CONFIG = '2020-01-19 04:40:41'  # '2020-01-03 11:42:35'#'2020-01-02 19:19:54'#'baseline'#'2020-01-01 20:03:30'#'2019-12-31 14:27:24'#'2019-12-18 18:53:17'
+TEST_UNET_WEIGHTS_PATH = os.path.join(EXPERIMENTS_FOLDER, TEST_UNET_CONFIG, 'bestcheckpoint.pth')
+TEST_UNET_REFINED_CONFIG = '2019-12-18 18:53:17'
+TEST_UNET_REFINED_WEIGHTS_PATH = os.path.join(EXPERIMENTS_FOLDER, TEST_UNET_REFINED_CONFIG, 'bestcheckpoint.pth')
+RAW_MUSDB_PATH = os.path.join(MUSDB_FOLDER_PATH, 'musdb18')
+MUSDB_WAVS_FOLDER_PATH = os.path.join(MUSDB_FOLDER_PATH, 'musdb18_wavs')
+ENERGY_PROFILE_FOLDER = os.path.join(MUSDB_FOLDER_PATH, 'energy_profile')
+MUSDB_SPLITS_PATH = os.path.join(MUSDB_FOLDER_PATH, 'musdbsplit')
+MUSDB_SPLITS_AUG_PATH = os.path.join(MUSDB_FOLDER_PATH, 'musdbaug')
+CHUNKS_PATH = os.path.join(MUSDB_FOLDER_PATH, 'musdb_chunks')
+SPECTROGRAMS_PATH = os.path.join(MUSDB_FOLDER_PATH, 'musdb_spectrograms')
+SOURCE_MIX_PATH = os.path.join(MUSDB_FOLDER_PATH, 'musdb_smix')
+STEMS_PATH = os.path.join(MUSDB_FOLDER_PATH, 'musdb_stems')
+RECON_GT_PATH = os.path.join(MUSDB_FOLDER_PATH, 'musdbGT')
+SOURCE_ESTIMATES_PATH = os.path.join(MUSDB_FOLDER_PATH, 'eval')
+TEST_DATA_PATH = os.path.join(RAW_MUSDB_PATH, 'test')
+TEST_SPEC_DATA_PATH = os.path.join(MUSDB_SPLITS_PATH, 'test')
+TEST_MAPPINGS_PATH = os.path.join(os.path.dirname(SEPERATED_OUTPUT_PATH), 'test_mappings.npy')
 
-SOURCES_SUBSET_ID=[SOURCES.index(i) for i in SOURCES_SUBSET]
-SAVE_SEGMENTS=True
-ENERGY_THRESHOLD=0
+SOURCES_SUBSET_ID = [SOURCES.index(i) for i in SOURCES_SUBSET]
+SAVE_SEGMENTS = True
+ENERGY_THRESHOLD = 0
 
-FILTERED_SAMPLE_PATHS=os.path.join(MUSDB_FOLDER_PATH,TYPE+'_filtered')
-
-
+FILTERED_SAMPLE_PATHS = os.path.join(MUSDB_FOLDER_PATH, TYPE + '_filtered')
